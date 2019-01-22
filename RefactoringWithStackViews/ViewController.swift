88df3df0ca8057dbc91a878710253bff3d82acd1
Nicholas Meschke <nicholas.meschke@runtastic.com>
2019-01-22 09:20:43 +0100

    Initial Commit

---
+++ RefactoringWithStackViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RefactoringWithStackViews
+//
+//  Created by Nicholas Meschke on 22.01.19.
+//  Copyright © 2019 Nicholas Meschke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
